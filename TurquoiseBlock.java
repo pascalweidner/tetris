b42b8d9912c12510daf1d69779c54e27ff4f87d4
Pascal Weidner <87229144+NobodyGames@users.noreply.github.com>
2021-09-29 21:52:12 +0200

    Create TurquoiseBlock.java

---
+++ TurquoiseBlock.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class TurquoiseBlock extends Blocks{
+    public TurquoiseBlock(int width, int pos) {
+        super(width, pos);
+        midY = 0;
+        generateBlock();
+    }
+
+    @Override
+    protected void generateBlock() {
+        super.blocks.add(new Block(new Color(4,154,152), midX, 0, blockWidth ));
+        super.blocks.add(new Block(new Color(4,154,152), midX+blockWidth, 0, blockWidth ));
+        super.blocks.add(new Block(new Color(4,154,152), midX, midY + blockWidth, blockWidth ));
+        super.blocks.add(new Block(new Color(4,154,152), midX + blockWidth, midY + blockWidth, blockWidth ));
+    }
+
+    @Override
+    protected void turn() {
+
+    }
+}
