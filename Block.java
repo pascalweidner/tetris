3fdb2368acee2d4c0b6121ede2183d220563babe
Pascal Weidner <87229144+NobodyGames@users.noreply.github.com>
2021-09-29 21:50:09 +0200

    Create Block.java

---
+++ Block.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+
+public class Block {
+    private Color color;
+    private final int width;
+    private int x;
+    private int y;
+
+    public Block(Color color, int xPosition, int yPosition, int width) {
+        this.color = color;
+        this.x = xPosition;
+        this.y = yPosition;
+        this.width = width;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, width, width);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
