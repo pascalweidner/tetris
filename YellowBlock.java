0fd9d18ad832e3c1fa63da6246274d45cbf4ecb9
Pascal Weidner <87229144+NobodyGames@users.noreply.github.com>
2021-10-06 21:26:53 +0200

    Update YellowBlock.java

--- YellowBlock.java
+++ YellowBlock.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 
 public class YellowBlock extends Blocks{
-    public YellowBlock(int width, int pos) {
-        super(width, pos);
+    public YellowBlock(int width, int pos, int height) {
+        super(width, pos, 'l', height);
         super.midY = 0;
         generateBlock();
     }
@@ -17,6 +17,42 @@ public class YellowBlock extends Blocks{
 
     @Override
     protected void turn() {
+        if(direction == 'l') {
+            blocks.get(0).setY(blocks.get(0).getY() - 2 * blockWidth);
+            blocks.get(1).setY(blocks.get(1).getY() - blockWidth);
+            blocks.get(1).setX(blocks.get(1).getX() + blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() - blockWidth);
+            blocks.get(3).setY(blocks.get(3).getY() + blockWidth);
+            super.direction = 'u';
+        }
+        else if(direction == 'u') {
+            blocks.get(0).setX(blocks.get(0).getX() + 2 * blockWidth);
+            blocks.get(1).setY(blocks.get(1).getY() + blockWidth);
+            blocks.get(1).setX(blocks.get(1).getX() + blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() - blockWidth);
+            blocks.get(3).setY(blocks.get(3).getY() - blockWidth);
+            super.direction = 'r';
+        }
+        else if(direction == 'r') {
+            blocks.get(0).setY(blocks.get(0).getY() + 2 * blockWidth);
+            blocks.get(1).setY(blocks.get(1).getY() + blockWidth);
+            blocks.get(1).setX(blocks.get(1).getX() - blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() + blockWidth);
+            blocks.get(3).setY(blocks.get(3).getY() - blockWidth);
+            super.direction = 'd';
+        }
+        else if(direction == 'd') {
+            blocks.get(0).setX(blocks.get(0).getX() - 2 * blockWidth);
+            blocks.get(1).setY(blocks.get(1).getY() - blockWidth);
+            blocks.get(1).setX(blocks.get(1).getX() - blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() + blockWidth);
+            blocks.get(3).setY(blocks.get(3).getY() + blockWidth);
+            super.direction = 'l';
+        }
+    }
 
+    @Override
+    void setMidY(int mid) {
+        midY = mid;
     }
 }
