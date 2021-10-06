f46f2533097a1dc0365c198edfe3638295efbd03
Pascal Weidner <87229144+NobodyGames@users.noreply.github.com>
2021-10-06 21:23:55 +0200

    Update BlueBlock.java

--- BlueBlock.java
+++ BlueBlock.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 
 public class BlueBlock extends Blocks{
-    public BlueBlock(int width, int pos) {
-        super(width, pos);
+    public BlueBlock(int width, int pos, int height) {
+        super(width, pos, 'l', height);
         super.midY = 0;
         generateBlock();
     }
@@ -17,6 +17,46 @@ public class BlueBlock extends Blocks{
 
     @Override
     protected void turn() {
+        if(direction == 'l') {
+            blocks.get(0).setX(blocks.get(0).getX() + blockWidth);
+            blocks.get(0).setY(blocks.get(0).getY() - blockWidth);
+            blocks.get(2).setX(blocks.get(2).getX() - blockWidth);
+            blocks.get(2).setY(blocks.get(2).getY() + blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() - blockWidth * 2);
+            blocks.get(3).setY(blocks.get(3).getY() + blockWidth * 2);
+            direction = 'd';
+        }
+        else if(direction == 'd') {
+            blocks.get(0).setX(blocks.get(0).getX() + blockWidth);
+            blocks.get(0).setY(blocks.get(0).getY() + blockWidth);
+            blocks.get(2).setX(blocks.get(2).getX() - blockWidth);
+            blocks.get(2).setY(blocks.get(2).getY() - blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() - blockWidth * 2);
+            blocks.get(3).setY(blocks.get(3).getY() - blockWidth * 2);
+            direction = 'r';
+        }
+        else if(direction == 'r') {
+            blocks.get(0).setX(blocks.get(0).getX() - blockWidth);
+            blocks.get(0).setY(blocks.get(0).getY() + blockWidth);
+            blocks.get(2).setX(blocks.get(2).getX() + blockWidth);
+            blocks.get(2).setY(blocks.get(2).getY() - blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() + blockWidth * 2);
+            blocks.get(3).setY(blocks.get(3).getY() - blockWidth * 2);
+            direction = 'u';
+        }
+        else if (direction == 'u') {
+            blocks.get(0).setX(blocks.get(0).getX() - blockWidth);
+            blocks.get(0).setY(blocks.get(0).getY() - blockWidth);
+            blocks.get(2).setX(blocks.get(2).getX() + blockWidth);
+            blocks.get(2).setY(blocks.get(2).getY() + blockWidth);
+            blocks.get(3).setX(blocks.get(3).getX() + blockWidth * 2);
+            blocks.get(3).setY(blocks.get(3).getY() + blockWidth * 2);
+            direction = 'l';
+        }
+    }
 
+    @Override
+    void setMidY(int mid) {
+        midY = mid;
     }
 }
